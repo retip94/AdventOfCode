69d335000c5cbda22d4f16713fb555a1e63938d5
Piotr Piekielny <38405004+retip94@users.noreply.github.com>
2019-01-16 16:49:34 +0100

    d12_t1 completed

--- d12/src/main/java/Main.java
+++ d12/src/main/java/Main.java
@@ -1,16 +1,42 @@
 import java.util.List;
 
 public class Main {
-    String input = "#..#.#..##......###...###";
+    static String pots = "#..#.#..##......###...###";
+    static String[] patterns = {"...##","..#..",".#...",".#.#.",".#.##",".##..",".####","#.#.#","#.###","##.#.","##.##","###..","###.#","####."};
     public static void main(String[] args) {
+        String oldPots = "..."+pots+"....";
+        System.out.println(oldPots);
+        int nGenerations = 20;
+        for (int gen = 1; gen <= nGenerations; gen++) {
+            StringBuilder newPots = new StringBuilder();
+            newPots.append(checkNextGeneration(".." + oldPots.substring(0, 3)));
+            newPots.append(checkNextGeneration("." + oldPots.substring(0, 4)));
+            for (int i = 2; i < (oldPots.length() - 2); i++) {
+                newPots.append(checkNextGeneration(oldPots.substring(i - 2, i + 3)));
+            }
+            newPots.append("....");
+            oldPots = newPots.toString();
+            System.out.println(oldPots);
+        }
+        int potsValue = getPotsValue(oldPots);
+        System.out.println(potsValue);
 
     }
 
-    boolean[] inputToArray() {
-        boolean[] plantsState = new boolean[input.length()];
-        for (int i = 0; i < input.length(); i++) {
-            plantsState[i] = input.charAt(i) == '#';
+    static char checkNextGeneration(String subPots) {
+        for (String pattern : patterns) {
+            if (subPots.equals(pattern)) {
+                return '#';
+            }
         }
-        return plantsState;
+        return '.';
+    }
+
+    static int getPotsValue(String pots) {
+        int totalValue = 0;
+        for (int i = 0; i < pots.length(); i++) {
+            if(pots.charAt(i)=='#') totalValue += (i - 3);
+        }
+        return totalValue;
     }
 }
